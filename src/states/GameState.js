3668247628e90a3a4d9aa3cc0488fb9572a3cfc8
dongy7 <dongy7@gmail.com>
2016-12-27 14:27:23 -0500

    add brick initialization

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -2,19 +2,51 @@ import RainbowText from 'objects/RainbowText';
 
 class GameState extends Phaser.State {
   preload() {
-    this.game.load('ball', 'assets/ballGrey.png');
-    this.game.load('paddle', 'assets/paddleBlu.png');
+    this.game.load.image('ball', 'assets/ballGrey.png');
+    this.game.load.image('paddle', 'assets/paddleBlu.png');
+    this.game.load.image('blueBlock', 'assets/element_blue_square_glossy.png');
+    this.game.load.image('greenBlock', 'assets/element_green_square_glossy.png');
+    this.game.load.image('purpleBlock', 'assets/element_purple_cube_glossy.png');
+    this.game.load.image('redBlock', 'assets/element_red_square_glossy.png');
+    this.game.load.image('yellowBlock', 'assets/element_yellow_square_glossy.png');
+    this.game.load.image('greyBlock', 'assets/element_grey_square_glossy.png');
   }
 
-  create () {
+  create() {
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
 		const center = {
       x: this.game.world.centerX,
       y: this.game.world.centerY
     };
 
-		const text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
+    this.createBricks();
+  }
 
-		text.anchor.set(0.5);
+  createBricks() {
+    const brickInfo = {
+      width: 32,
+      height: 32,
+      paddingTop: 32,
+    };
+
+    const brickTypes = [
+      'redBlock',
+      'yellowBlock',
+      'greenBlock',
+      'blueBlock',
+      'purpleBlock',
+    ];
+
+    const bricks = this.game.add.group();
+    bricks.enableBody = true;
+
+    for (let i = 0; i < brickTypes.length; i++) {
+      for (let j = 0; j < this.game.width; j += brickInfo.width) {
+        const brick = bricks.create(j, i * brickInfo.height + brickInfo.paddingTop, brickTypes[i]);
+        brick.body.immovable = true;
+      }
+    }
   }
 }
 
