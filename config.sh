de6230e952ed7483dbf938d8b4847df47960754d
dongy7 <dongy7@gmail.com>
2016-12-28 17:20:07 -0500

    decrypt ssh key before pushing

--- config.sh
+++ config.sh
@@ -2,3 +2,13 @@
 
 git config --global user.email "dongy7@gmail.com"
 git config --global user.name "Dong Yeop Lee"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis_rsa.pub.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
