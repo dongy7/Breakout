ba8d4050bc76c1a661c88f0399a4daeede1ebcbc
dongy7 <dongy7@gmail.com>
2016-12-27 19:53:28 -0500

    fix indent

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import GameState from 'states/GameState';
 
 class Game extends Phaser.Game {
-	constructor() {
-		super(1280, 720, Phaser.AUTO, 'content', null);
-		this.state.add('GameState', GameState, false);
-		this.state.start('GameState');
-	}
+  constructor() {
+    super(1280, 720, Phaser.AUTO, 'content', null);
+    this.state.add('GameState', GameState, false);
+    this.state.start('GameState');
+  }
 }
 
 new Game();
